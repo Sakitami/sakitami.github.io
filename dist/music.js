6bfa40d96a1d728351f5ef43c8dbacdc2908c891
sakitami <Sakitam2373@gmail.com>
2019-07-01 03:04:14 +0800

    Site updated: 2019-07-01 03:04:04

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 01 2019 00:33:04 GMT+0800 (GMT+08:00)
+// build time:Mon Jul 01 2019 02:42:08 GMT+0800 (GMT+08:00)
 const ap=new APlayer({container:document.getElementById("aplayer"),autoplay:false,audio:[{name:"一花依世界",artist:"洛天依",url:"http://www.ytmp3.cn/down/51653.mp3",cover:"https://tvax1.sinaimg.cn/large/005BYqpggy1g4bimwpcaij30zk0spn2d.jpg"},{name:"不老不死【异托邦LOVETOPIA】",artist:"洛天依",url:"dist/不老不死.mp3",cover:"https://tvax1.sinaimg.cn/large/005BYqpggy1g4bix0p6p9j30ee0k2wq9.jpg"}]});
 //rebuild by neat 
--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 01 2019 01:39:53 GMT+0800 (GMT+08:00)
+// build time:Mon Jul 01 2019 02:42:08 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/images/favicon-32x32-next.png");document.title="\\(￣︶￣*\\))点这里点这里〜";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/images/favicon-32x32-next.png");document.title="（ฅ>ω<*ฅ）噫~我中了!"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
