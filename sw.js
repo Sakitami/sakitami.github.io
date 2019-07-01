456afea82d1c62a68db54b368e64ba5d0a41801d
sakitami <Sakitam2373@gmail.com>
2019-07-01 19:21:48 +0800

    Site updated: 2019-07-01 19:21:37

--- sw.js
+++ sw.js
@@ -26,15 +26,15 @@ var precacheUrls = [];
 
   precacheUrls.push('https://blog.skihome.xyz/posts/8ad3eb9f/');
 
+  precacheUrls.push('/posts/41b9663e/');
+
   precacheUrls.push('/posts/b79c5602/');
-
-  precacheUrls.push('/posts/cf71037e/');
-
-  precacheUrls.push('/posts/770f7eef/');
 
   precacheUrls.push('/posts/7968d939/');
 
-  precacheUrls.push('/posts/41b9663e/');
+  precacheUrls.push('/posts/770f7eef/');
+
+  precacheUrls.push('/posts/cf71037e/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
