5926d8aeb326d517ef42595f9e6b39ef0d4c68a3
Sakitami <sakitam2373@gmail.com>
2019-07-29 16:20:34 +0800

    Site updated: 2019-07-29 16:20:31

--- dist/music.js
+++
@@ -1,3 +0,0 @@
-// build time:Tue Jul 02 2019 12:18:47 GMT+0800 (GMT+08:00)
-const ap=new APlayer({container:document.getElementById("aplayer"),autoplay:false,audio:[{name:"不老不死【异托邦LOVETOPIA】",artist:"洛天依",url:"dist/不老不死.mp3",cover:"https://tvax1.sinaimg.cn/large/005BYqpggy1g4bix0p6p9j30ee0k2wq9.jpg"}]});
-//rebuild by neat 
--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sat Jul 06 2019 21:19:27 GMT+0800 (中国标准时间)
+// build time:Mon Jul 29 2019 16:00:48 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/images/favicon-32x32-next.png");document.title="\\(￣︶￣*\\))点这里点这里〜";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/images/favicon-32x32-next.png");document.title="（ฅ>ω<*ฅ）噫~我中了!"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
