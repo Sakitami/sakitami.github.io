d9d7d9c44357e4c65ccd9886ce4fda47004dda37
Sakitami <sakitam2373@gmail.com>
2019-08-03 00:25:18 +0800

    Site updated: 2019-08-03 00:25:13

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Sat Aug 03 2019 00:12:17 GMT+0800 (GMT+08:00)
-const ap=new APlayer({container:document.getElementById("aplayer"),autoplay:false,audio:[{name:"Pick Me Up",artist:"初音未来",url:"PickMeUp.mp3",cover:"https://tvax1.sinaimg.cn/large/005BYqpggy1g4bimwpcaij30zk0spn2d.jpg"}]});
+// build time:Sat Aug 03 2019 00:25:02 GMT+0800 (GMT+08:00)
+const ap=new APlayer({container:document.getElementById("aplayer"),autoplay:false,audio:[{name:"Pick Me Up",artist:"初音未来",url:"/dist/PickMeUp.mp3",cover:"https://tvax1.sinaimg.cn/large/005BYqpggy1g4bimwpcaij30zk0spn2d.jpg"}]});
 //rebuild by neat 
--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sat Aug 03 2019 00:12:17 GMT+0800 (GMT+08:00)
+// build time:Sat Aug 03 2019 00:23:21 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/images/favicon-32x32-next.png");document.title="\\(￣︶￣*\\))点这里点这里〜";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/images/favicon-32x32-next.png");document.title="（ฅ>ω<*ฅ）噫~我中了!"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
