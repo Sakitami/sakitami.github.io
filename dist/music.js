26d091e7ebdecbacb0d79304a93e02c43860bf1a
Sakitami <sakitam2373@gmail.com>
2019-08-03 02:10:29 +0800

    Site updated: 2019-08-03 02:10:23

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Sat Aug 03 2019 01:59:00 GMT+0800 (GMT+08:00)
+// build time:Sat Aug 03 2019 02:10:02 GMT+0800 (GMT+08:00)
 const ap=new APlayer({container:document.getElementById("aplayer"),autoplay:false,audio:[{name:"Pick Me Up",artist:"初音未来",url:"/dist/PickMeUp.mp3",cover:"/dist/cover.jpg"}]});
 //rebuild by neat 
