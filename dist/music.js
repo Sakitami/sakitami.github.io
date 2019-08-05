15b2320b6a7f5796cec752402099aeff657ce73d
Sakitami <sakitam2373@gmail.com>
2019-08-05 12:12:04 +0800

    Site updated: 2019-08-05 12:11:57

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Mon Aug 05 2019 10:06:37 GMT+0800 (GMT+08:00)
+// build time:Mon Aug 05 2019 12:05:40 GMT+0800 (GMT+08:00)
 const ap=new APlayer({container:document.getElementById("aplayer"),autoplay:false,audio:[{name:"Starlight",artist:"洛天依",url:"https://music.163.com/song/media/outer/url?id=1377061819.mp3",cover:"https://raw.githubusercontent.com/Sakitami/Figurebed/master/img/109951164204349759.webp"},{name:"Pick Me Up",artist:"初音未来",url:"https://music.163.com/song/media/outer/url?id=1316563139.mp3",cover:"https://raw.githubusercontent.com/Sakitami/Figurebed/master/img/cover.webp"}]});
 //rebuild by neat 
