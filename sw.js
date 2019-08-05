93ab8ad62a61e2c11f4819635dda62e6ce0f3d11
Sakitami <sakitam2373@gmail.com>
2019-08-05 10:16:22 +0800

    Site updated: 2019-08-05 10:16:14

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ var precacheUrls = [];
 
   precacheUrls.push('https://blog.skihome.xyz/posts/8ad3eb9f/');
 
-  precacheUrls.push('/posts/c1d4be5c/');
+  precacheUrls.push('/posts/d75bc54e/');
 
   precacheUrls.push('/posts/18f169a8/');
 
