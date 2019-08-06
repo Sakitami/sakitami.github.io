4274287d19c999ce6e5231cc063c726a29a1215d
Sakitami <sakitam2373@gmail.com>
2019-08-06 19:44:40 +0800

    Site updated: 2019-08-06 19:44:36

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Tue Aug 06 2019 11:42:11 GMT+0800 (GMT+08:00)
+// build time:Tue Aug 06 2019 19:04:55 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/images/favicon-32x32-next.png");document.title="\\(￣︶￣*\\))点这里点这里〜";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/images/favicon-32x32-next.png");document.title="（ฅ>ω<*ฅ）噫~我中了!"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
