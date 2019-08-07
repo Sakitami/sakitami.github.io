2891565eabdb0396832a0dfd533e9ab725705574
Sakitami <sakitam2373@gmail.com>
2019-08-07 18:14:28 +0800

    Site updated: 2019-08-07 18:14:24

--- js/src/hitokoto.js
+++ js/src/hitokoto.js
@@ -1,3 +1,3 @@
-// build time:Wed Aug 07 2019 14:58:46 GMT+0800 (GMT+08:00)
+// build time:Wed Aug 07 2019 18:14:20 GMT+0800 (GMT+08:00)
 window.onload=function(){var t=document.querySelector(".hitokoto");var e=document.querySelector(".from");o();function o(){gethi=new XMLHttpRequest;gethi.open("GET","https://v1.hitokoto.cn");gethi.send();gethi.onreadystatechange=function(){if(gethi.readyState===4&&gethi.status===200){var o=JSON.parse(gethi.responseText);t.innerHTML=o.hitokoto;e.innerHTML="from: <b>"+o.from+"</b>"}}}};
 //rebuild by neat 
--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ var precacheUrls = [];
 
   precacheUrls.push('https://blog.skihome.xyz/posts/8ad3eb9f/');
 
-  precacheUrls.push('/posts/c1d4be5c/');
+  precacheUrls.push('/posts/7968d939/');
 
   precacheUrls.push('/posts/18f169a8/');
 
@@ -34,7 +34,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/be19145/');
 
-  precacheUrls.push('/posts/45729de/');
+  precacheUrls.push('/posts/18766635/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
