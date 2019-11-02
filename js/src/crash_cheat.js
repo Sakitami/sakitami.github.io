2d250f254c10dd574459e6fe33c265e8a19a8301
Sakitami <sakitam2373@gmail.com>
2019-11-02 18:40:22 +0800

    Site updated: 2019-11-02 18:40:17

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sat Oct 26 2019 19:37:11 GMT+0800 (GMT+08:00)
+// build time:Sat Nov 02 2019 18:40:01 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/images/favicon-32x32-next.png");document.title="\\(￣︶￣*\\))点这里点这里〜";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/images/favicon-32x32-next.png");document.title="（ฅ>ω<*ฅ）噫~我中了!"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
--- js/src/hitokoto.js
+++ js/src/hitokoto.js
@@ -1,3 +1,3 @@
-// build time:Sat Oct 26 2019 19:37:11 GMT+0800 (GMT+08:00)
+// build time:Sat Nov 02 2019 18:40:01 GMT+0800 (GMT+08:00)
 window.onload=function(){var t=document.querySelector(".hitokoto");var e=document.querySelector(".from");o();function o(){gethi=new XMLHttpRequest;gethi.open("GET","https://v1.hitokoto.cn");gethi.send();gethi.onreadystatechange=function(){if(gethi.readyState===4&&gethi.status===200){var o=JSON.parse(gethi.responseText);t.innerHTML=o.hitokoto;e.innerHTML="from: <b>"+o.from+"</b>"}}}};
 //rebuild by neat 
